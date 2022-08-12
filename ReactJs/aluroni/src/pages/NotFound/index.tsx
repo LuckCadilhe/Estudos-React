f9c0d0d81bc36f350d8658f1492ea0c2e33ac9a4
Lucas Cadilhe <lucas_cadilhe@hotmail.com>
2022-08-12 14:25:05 -0300

    Finalizando estudo de rotas

--- ReactJs/aluroni/src/pages/NotFound/index.tsx
+++ ReactJs/aluroni/src/pages/NotFound/index.tsx
@@ -5,18 +5,18 @@ import stylesTema from 'styles/Tema.module.scss';
 import { useNavigate } from 'react-router-dom';
 
 export default function NotFound() {
-    const navigate = useNavigate();
-    return (
-        <div className={classNames({
-            [styles.container]: true,
-            [stylesTema.container]: true
-        })}>
-            <div className={styles.voltar}>
-                <button onClick={() => navigate(-1)}>
-                    {'< Voltar'}
-                </button>
-            </div>
-            <NotFoundImage />
-        </div>
-    );
+  const navigate = useNavigate();
+  return (
+    <div className={classNames({
+      [styles.container]: true,
+      [stylesTema.container]: true
+    })}>
+      <div className={styles.voltar}>
+        <button onClick={() => navigate(-1)}>
+          {'< Voltar'}
+        </button>
+      </div>
+      <NotFoundImage />
+    </div>
+  );
 }
